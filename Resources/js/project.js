b9c2adfd0b046357d2494c867a028c507e2f7368
Paula <pc619@ic.ac.uk>
2021-10-20 11:59:17 +0100

    changes

--- Resources/js/project.js
+++ Resources/js/project.js
@@ -41,6 +41,42 @@ const find_prev_page = function(strCurrentPage, arrProjectArray) {
 
 const initFunct = function () {
 
+    $("#nanogallery-nintendo").nanogallery2({
+        items: [
+            { src: "Resources/img/nintendo_rev_img/final_render_button_box.png", srct: "Resources/img/nintendo_rev_img/final_render_button_box.png"},
+            { src: "Resources/img/nintendo_rev_img/mat_final_render-01.png", srct: "Resources/img/nintendo_rev_img/mat_final_render-01.png" },
+            { src: "Resources/img/nintendo_rev_img/pngchar_post_proc.jpg", srct: "Resources/img/nintendo_rev_img/pngchar_post_proc.jpg" },
+            { src: "Resources/img/nintendo_rev_img/button_box_exploded.png", srct: "Resources/img/nintendo_rev_img/button_box_exploded.png" },
+            { src: "Resources/img/nintendo_rev_img/matt_exploded_view-01.png", srct: "Resources/img/nintendo_rev_img/matt_exploded_view-01.png" },
+            { src: "Resources/img/nintendo_rev_img/user_guide.png", srct: "Resources/img/nintendo_rev_img/user_guide.png" },
+            { src: "Resources/img/nintendo_rev_img/packaging_exploded_view.png", srct: "Resources/img/nintendo_rev_img/packaging_exploded_view.png" }
+        ],
+
+        thumbnailWidth: "auto",
+        thumbnailHeight:  250,
+        thumbnailGutterWidth: 30,
+        thumbnailGutterHeight: 30,
+        thumbnailBorderHorizontal: 0,
+        thumbnailBorderVertical: 0,
+        thumbnailAlignment: "center",
+        viewerToolbar:    {
+            display:    false },
+        viewerTools:     {
+                topLeft:    "",
+                topRight:   "closeButton"
+        },
+        galleryTheme: {
+            theme: "light"
+          },
+        viewerTheme : { background: '#fff' },
+        icons: {
+            buttonClose: '<ion-icon  style="color: #000;" name="close-outline"></ion-icon>',   // icon for previous image (toolbar)
+            viewerImgNext: '<ion-icon style="font-size:48px; opacity: 50%; color: #000" name="chevron-forward-circle"></ion-icon>',
+            viewerImgPrevious: '<ion-icon style="font-size:48px; opacity: 50%; color: #000"" name="chevron-back-circle"></ion-icon>'
+        }
+    });
+
+
         $(".popup-modal").magnificPopup({
             type: 'iframe'
 
@@ -55,8 +91,6 @@ const initFunct = function () {
         "tetris.html",
         "angard.html",
         "quizzard.html",
-        "shazam_rebrand.html",
-        "first_portfolio.html",
         "grocity.html"
     ];
 
